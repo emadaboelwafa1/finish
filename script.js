ed859634087ae22e266b7f398a05cd34a86c73fa
emadaboelwafa1 <eaboelwfa5@gmail.com>
2024-11-24 13:39:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const locations = [
     { name: "Location 1", latitude: 31.215, longitude: 29.924, tolerance: 0.05 }, // لوكيشن 1
     { name: "Location 2", latitude: 30.500, longitude: 31.100, tolerance: 0.05 }, // لوكيشن 2
     { name: "Location 3", latitude: 32.000, longitude: 28.500, tolerance: 0.05 }, // لوكيشن 3
-    { name: "Your Custom Location", latitude: 31.215, longitude: 29.924, tolerance: 0.05 } // لوكيشن جديد
+     // لوكيشن القاهرة مع نطاق 20 كيلومتر مربع
+    { name: "Cairo (20 km radius)", latitude: 30.0444, longitude: 31.2357, tolerance: 0.18 } // القاهرة
 ];
 
 // دالة للتحقق من الموقع
